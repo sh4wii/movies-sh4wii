919d4373d229c45fb7eff47217c0b9302061de70
Sh4wii <131603396+sh4wii@users.noreply.github.com>
2023-09-10 11:08:32 +0530

    Update index.py

--- index.py
+++ index.py
@@ -14,8 +14,8 @@ bot = Bot(TOKEN)
 
 
 def welcome(update, context) -> None:
-    update.message.reply_text(f"Hello {update.message.from_user.first_name}, Welcome to SB Movies.\n"
-                              f"🔥 Download Your Favourite Movies For 💯 Free And 🍿 Enjoy it.")
+    update.message.reply_text(f"Hello {update.message.from_user.first_name}, Welcome to SH4WII MEDIA.\n"
+                              f"🔥 Download Your Favourite Movies For 💯 Free And Follow sh4wii on insta.")
     update.message.reply_text("👇 Enter Movie Name 👇")
 
 

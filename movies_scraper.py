c3f52fdc00b5f50cbaa730d54cd0df05894449da
Sh4wii <131603396+sh4wii@users.noreply.github.com>
2023-09-10 11:40:33 +0530

    Update movies_scraper.py

--- movies_scraper.py
+++ movies_scraper.py
@@ -7,15 +7,12 @@ api_key = "05ec289b4931dbc5c38de970e22ec240c72be101"
 def search_movies(query):
     movies_list = []
     movies_details = {}
-    website = BeautifulSoup(requests.get(f"https://moviesmod.cc/?s={query.replace(' ', '+')}").text, "html.parser")
-    
-    # Using 'a' as a common tag for links; adjust if necessary.
-    movies = website.find_all("a", {'class': 'js'})
-    
+    website = BeautifulSoup(requests.get(f"https://185.53.88.104/?s={query.replace(' ', '+')}").text, "html.parser")
+    movies = website.find_all("a", {'class': 'ml-mask jt'})
     for movie in movies:
         if movie:
             movies_details["id"] = f"link{movies.index(movie)}"
-            movies_details["title"] = movie.text
+            movies_details["title"] = movie.find("span", {'class': 'mli-info'}).text
             url_list[movies_details["id"]] = movie['href']
         movies_list.append(movies_details)
         movies_details = {}
@@ -24,23 +21,23 @@ def search_movies(query):
 def get_movie(query):
     movie_details = {}
     movie_page_link = BeautifulSoup(requests.get(f"{url_list[query]}").text, "html.parser")
-    
-    title = movie_page_link.find("a", {'class': 'js'}).text  # Adjust if the tag is different on the details page.
-    movie_details["title"] = title
-    
-    # If the image has the same class or a different one, adjust accordingly
-    img = movie_page_link.find("img", {'class': 'js'})['src']
-    movie_details["img"] = img
-    
-    # Assuming movie download links also use the same class
-    links = movie_page_link.find_all("a", {'class': 'js'})
-    
-    final_links = {}
-    for i in links:
-        url = f"https://urlshortx.com/api?api={api_key}&url={i['href']}"
-        response = requests.get(url)
-        link = response.json()
-        final_links[f"{i.text}"] = link['shortenedUrl']
-    movie_details["links"] = final_links
-    
+    if movie_page_link:
+        title = movie_page_link.find("div", {'class': 'mvic-desc'}).h3.text
+        movie_details["title"] = title
+        img = movie_page_link.find("div", {'class': 'mvic-thumb'})['data-bg']
+        movie_details["img"] = img
+        links = movie_page_link.find_all("a", {'rel': 'noopener', 'data-wpel-link': 'internal'})
+        final_links = {}
+        for i in links:
+            url = f"https://urlshortx.com/api?api={api_key}&url={i['href']}"
+            response = requests.get(url)
+            link = response.json()
+            final_links[f"{i.text}"] = link['shortenedUrl']
+        movie_details["links"] = final_links
     return movie_details
+
+requirements.txt
+beautifulsoup4==4.11.1
+python_telegram_bot==13.14
+requests==2.28.1
+Flask==2.2.2
